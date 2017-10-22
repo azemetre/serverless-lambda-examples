ccd65d22fa2d3f62a9b1955edb9bab61a022d54e
Aaron Zemetres <azemetre@gmail.com>
2017-10-21 20:47:36 -0400

    feat: created lambda functions to email reminders using ses

--- serverless-reminders/handler.js
+++ serverless-reminders/handler.js
@@ -1,16 +1,87 @@
+require('dotenv').config();
+
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+module.exports.sendReminderDaily = (event, context, callback) => {
+    
+    var AWS = require('aws-sdk');
+    AWS.config.update({region:'us-east-1'});
+    var ses = new AWS.SES();
+    var fs = require('fs');
 
-  callback(null, response);
+    var emailHtml = fs.readFileSync('./dailyReminder.html', 'utf-8');
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    var toAndFromAddress = process.env.VERIFIED_AWS_EMAIL;
+    var params = {
+        Destination: {
+            ToAddresses: [toAndFromAddress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8", 
+                    Data: emailHtml
+                }, 
+                Text: {
+                    Charset: "UTF-8", 
+                    Data: "Remember to continue helping flossing your teeth!"
+                }
+            }, 
+            Subject: {
+                Charset: "UTF-8", 
+                Data: "Dental Hygiene Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAddress],
+        Source: toAndFromAddress, 
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack); 
+        // successful response
+        else callback(null, data);
+    }); 
+};
+
+module.exports.sendReminderWeekend = (event, context, callback) => {
+    
+    var AWS = require('aws-sdk');
+    AWS.config.update({region:'us-east-1'});
+    var ses = new AWS.SES();
+    var fs = require('fs');
+
+    var emailHtml = fs.readFileSync('./weekendReminder.html', 'utf-8');
+
+    var toAndFromAddress = 'replace.with.verified.ses.email@aws.com'
+    var params = {
+        Destination: {
+            ToAddresses: [toAndFromAddress]
+        },
+        Message: {
+            Body: {
+                Html: {
+                    Charset: "UTF-8", 
+                    Data: emailHtml
+                }, 
+                Text: {
+                    Charset: "UTF-8", 
+                    Data: "Here's a weekend reminder: floss your damn teeth."
+                }
+            }, 
+            Subject: {
+                Charset: "UTF-8", 
+                Data: "Dental Hygiene Reminder"
+            }
+        },
+        ReplyToAddresses: [toAndFromAddress],
+        Source: toAndFromAddress, 
+    };
+
+    ses.sendEmail(params, function(err, data) {
+        // an error occurred
+        if (err) console.log(err, err.stack); 
+        // successful response
+        else callback(null, data);
+    }); 
 };
